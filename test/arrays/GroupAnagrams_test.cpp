e90f5f82cc175ceb9d88fc613767bba9a998f55c
William Hammond <william.t.hammond@gmail.com>
2022-05-04 09:46:49 -0400

    Fix GroupAnagram tests

--- test/arrays/GroupAnagrams_test.cpp
+++ test/arrays/GroupAnagrams_test.cpp
@@ -4,25 +4,23 @@
 #include<algorithm>
 
 TEST(GroupAnagrams, exmaple) {
-    std::vector<std::string> input = {"eat", "tea", "tan", "ate", "nat", "bat"};
-    std::vector<std::vector<std::string>> actual = GroupAnagrams::solve(input);
     std::vector<std::vector<std::string>> expected = {{"bat"},
-                                                      {"nat", "tan"},
+                                                      {"tan", "nat"},
                                                       {"ate", "eat", "tea"}};
-    std::vector<std::string> test = {"tan", "nat"};
-    std::sort(test.begin(), test.end());
-    for (auto group: actual) {
+    for (std::vector<std::string> &group: expected) {
         std::sort(group.begin(), group.end());
     }
-    std::sort(actual.begin(), actual.end(),
+    std::sort(expected.begin(), expected.end(),
               [](const std::vector<std::string> &a, const std::vector<std::string> &b) {
                   return a.size() - b.size();
               });
 
-    for (auto group: expected) {
+    std::vector<std::string> input = {"eat", "tea", "tan", "ate", "nat", "bat"};
+    std::vector<std::vector<std::string>> actual = GroupAnagrams::solve(input);
+    for (std::vector<std::string> &group: actual) {
         std::sort(group.begin(), group.end());
     }
-    std::sort(expected.begin(), expected.end(),
+    std::sort(actual.begin(), actual.end(),
               [](const std::vector<std::string> &a, const std::vector<std::string> &b) {
                   return a.size() - b.size();
               });

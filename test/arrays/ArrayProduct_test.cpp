9f2f4946d598bafc4e9e5ec44441ec26c4215adf
William Hammond <william.t.hammond@gmail.com>
2022-05-06 09:12:49 -0400

    Solves top-k-frequent-elements

---
+++ test/arrays/ArrayProduct_test.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include "../../src/arrays/public/ArrayProduct.h"
+#include <vector>
+
+TEST(ArrayProduct, solves) {
+    std::vector<int> input = {1, 2, 3};
+    auto actual = ArrayProduct::solve(input);
+    std::vector<int> expected = {6, 3, 2};
+    EXPECT_EQ(expected, actual);
+
+    input = {1, 2, 3, 4};
+    actual = ArrayProduct::solve(input);
+    expected = {24, 12, 8, 6};
+    EXPECT_EQ(expected, actual);
+
+    input = {-1, 1, 0, -3, 3};
+    actual = ArrayProduct::solve(input);
+    expected = {0, 0, 9, 0, 0};
+    EXPECT_EQ(expected, actual);
+
+    input = {1, 1, 1, 1};
+    actual = ArrayProduct::solve(input);
+    expected = {1, 1, 1, 1};
+    EXPECT_EQ(expected, actual);
+}

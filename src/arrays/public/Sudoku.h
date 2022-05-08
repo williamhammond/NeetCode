d28b5ae77f6f5fadf3d1ac6b4495f30c15d8906c
William Hammond <william.t.hammond@gmail.com>
2022-05-08 14:44:47 -0400

    Solves valid-sudoku

---
+++ src/arrays/public/Sudoku.h
@@ -0,0 +1,14 @@
+
+#ifndef NEETCODE_SUDOKU_H
+#define NEETCODE_SUDOKU_H
+
+#include <vector>
+
+class Sudoku {
+
+public:
+    static bool solve(std::vector<std::vector<char>> &board);
+};
+
+
+#endif //NEETCODE_SUDOKU_H

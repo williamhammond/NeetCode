3451978343cce7ff606f131704341a0ca1d45656
William Hammond <william.t.hammond@gmail.com>
2022-05-11 20:11:23 -0400

    Solves valid palindrome

---
+++ src/pointers/private/ValidPalindrome.cpp
@@ -0,0 +1,15 @@
+#include "pointers/public/ValidPalindrome.h"
+#include <cctype>
+#include <functional>
+
+bool ValidPalindrome::solve(std::string s) {
+    s.erase(std::remove_if(s.begin(), s.end(), &ispunct), s.end());
+    s.erase(std::remove_if(s.begin(), s.end(), &isspace), s.end());
+    for (std::size_t i = 0; i < s.size() / 2; i++) {
+        std::size_t j = s.size() - i - 1;
+        if (std::tolower(s[i]) != std::tolower(s[j])) {
+            return false;
+        }
+    }
+    return true;
+}
---
+++ src/pointers/public/ValidPalindrome.h
@@ -0,0 +1,14 @@
+
+#ifndef NEETCODE_VALIDPALINDROME_H
+#define NEETCODE_VALIDPALINDROME_H
+
+#include <string>
+
+class ValidPalindrome {
+public:
+    static bool solve(std::string s);
+
+};
+
+
+#endif //NEETCODE_VALIDPALINDROME_H

94a70dd9d35cfe48c33bdc202ceb2c07e0d204f6
William Hammond <william.t.hammond@gmail.com>
2022-05-14 09:36:04 -0400

    Solves container-with-most-water

---
+++ src/pointers/WaterContainer.h
@@ -0,0 +1,15 @@
+
+#ifndef NEETCODE_WATERCONTAINER_H
+#define NEETCODE_WATERCONTAINER_H
+
+#include <vector>
+
+
+class WaterContainer {
+public:
+    static std::size_t solve(std::vector<int> &height);
+
+};
+
+
+#endif //NEETCODE_WATERCONTAINER_H

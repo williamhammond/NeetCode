1c1f1946d3f7846865ab93b3ed02873a4080c910
William Hammond <william.t.hammond@gmail.com>
2022-08-10 11:27:17 -0400

    Solves searchRotated

--- src/binary_search/BinarySearch.h
+++ src/binary_search/BinarySearch.h
@@ -7,6 +7,7 @@ class BinarySearch {
  public:
   static int search(std::vector<int>& nums, int target);
   static bool searchMatrix(std::vector<std::vector<int>>& matrix, int target);
+  static int searchRotated(std::vector<int>& nums, int target);
 };
 
 #endif  // NEETCODE_SRC_BINARY_SEARCH_BINARYSEARCH_H_
